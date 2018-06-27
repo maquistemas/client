ac2c5307ecf09be42f855526a64cdffb4a2c9ed7
john <maquistemas@gmail.com>
2018-06-27 12:04:47 -0500

    Registrar usuario

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -15,9 +15,11 @@ export class UserService{
 	}
 
 
-	register(user_to_register){
-		console.log(user_to_register);
-		console.log(this.url);
+	register(user: User): Observable<any>{
+		let params = JSON.stringify(user);
+		let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+		return this._http.post(this.url+'register', params, {headers:headers});
 	}
 
 }
