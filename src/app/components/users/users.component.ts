adc0aa4a671a95f2f2d6788fc68594884c64b465
john <maquistemas@gmail.com>
2018-06-29 05:36:32 -0500

    Servicios y usuarios paginados

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../../services/user.service';
 import { GLOBAL } from '../../services/global';
 
 @Component({
-	select: 'users',
+	selector: 'users',
 	templateUrl: './users.component.html',
 	providers: [UserService]
 	})
@@ -13,6 +13,13 @@ export class UsersComponent implements OnInit{
 	public title: string;
 	public identity;
 	public token;
+	public page;
+	public next_page;
+	public prev_page;
+	public total;
+	public pages;
+	public users: User[];
+	public status: string;
 
 	constructor(
 		private _route: ActivatedRoute,
@@ -26,6 +33,56 @@ export class UsersComponent implements OnInit{
 
 	ngOnInit(){
 		console.log('users.component ha sido cargado');
+		this.actualPage();
+	}
+
+
+	actualPage(){
+		this._route.params.subscribe(params => {
+			let page = params['page'];
+			this.page = page;
+
+			if(!page){
+				page = 1;
+			}else{
+				this.next_page = page+1;
+				this.prev_page = page-1;
+
+				if(this.prev_page <= 0){
+					this.prev_page = 1;
+				}
+			}
+
+			//devolver listado de usuarios
+			this.getUsers(page);
+		});
+	}
+
+
+	getUsers(page){
+		this._userService.getUsers(page).subscribe(
+			response => {
+				if(!response.users){
+					this.status = 'error';
+				}else{
+					this.total = response.total;
+					this.users = response.users;
+					this.pages = response.pages;
+					if(page > this.pages){
+						this._router.navigate(['/gente',1]);
+					}
+				}
+
+				},
+			error => {
+				var errorMessage = <any>error;
+				console.log(errorMessage);
+
+				if(errorMessage != null){
+					this.status = 'error';
+				}
+			}
+			)
 	}
 
 }
