a64c7971c2ce6504acee0d442e9c589ad8006039
john <maquistemas@gmail.com>
2018-06-28 23:40:36 -0500

    Subir Avatar

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import { UploadService } from '../../services/upload.service';
+import { GLOBAL } from '../../services/global';
 
 @Component({
 	selector: 'user-edit',
 	templateUrl: './user-edit.component.html',
-	providers: [UserService]
+	providers: [UserService, UploadService]
 	})
 export class UserEditComponent implements OnInit{
 	public title: string;
@@ -14,16 +16,19 @@ export class UserEditComponent implements OnInit{
 	public identity;
 	public token;
 	public status: string;
+	public url: string;
 
 	constructor(
 		private _route: ActivatedRoute,
 		private _router: Router,
-		private _userService: UserService
+		private _userService: UserService,
+		private _uploadService: UploadService
 		){
 		this.title = 'Actualizar mis datos';
 		this.user = this._userService.getIdentity();
 		this.identity = this.user;
 		this.token = this._userService.getToken();
+		this.url = GLOBAL.url;
 	}
 
 
@@ -45,6 +50,12 @@ export class UserEditComponent implements OnInit{
 						this.identity = this.user;
 
 						//SUBIDA DE IMAGEN DE USUARIO
+						this._uploadService.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload, this.token, 'image')
+										   .then((result: any) => {
+										   		this.user.image = result.user.image;
+										   		localStorage.setItem('identity', JSON.stringify(this.user));
+
+										   	});
 						
 					}
 				},
@@ -56,6 +67,13 @@ export class UserEditComponent implements OnInit{
 				}
 			}
 			);
+	}
+
+
+	public filesToUpload: Array<File>;
+	fileChangeEvent(fileInput: any){
+		this.filesToUpload = <Array<File>>fileInput.target.files;
+		console.log(this.filesToUpload);
 	}
 
 
