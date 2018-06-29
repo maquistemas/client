da7fa808ad0becf070ff5f151a95ea208e10e608
john <maquistemas@gmail.com>
2018-06-29 04:47:28 -0500

    Página, componente y ruta de Gente

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { GLOBAL } from '../../services/global';
+
+@Component({
+	select: 'users',
+	templateUrl: './users.component.html',
+	providers: [UserService]
+	})
+export class UsersComponent implements OnInit{
+	public title: string;
+	public identity;
+	public token;
+
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private _userService: UserService
+		){
+			this.title = 'Gente';
+			this.identity = this._userService.getIdentity();
+			this.token = this._userService.getToken();
+		}
+
+	ngOnInit(){
+		console.log('users.component ha sido cargado');
+	}
+
+}
