2119353f6e266dc0f27a3d0fca4ec84b337a04cf
john <maquistemas@gmail.com>
2018-07-01 12:31:09 -0500

    Crear publicaciones

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { GLOBAL } from '../../services/global';
 import { Publication } from '../../models/publication';
+import { PublicationService } from '../../services/publication.service';
 
 @Component({
 	selector: 'sidebar',
 	templateUrl: './sidebar.component.html',
-	providers: [UserService]
+	providers: [UserService, PublicationService]
 	})
 export class SidebarComponent implements OnInit{
 	public identity;
@@ -17,7 +18,8 @@ export class SidebarComponent implements OnInit{
 	public publication: Publication;
 
 	constructor(
-		private _userService: UserService
+		private _userService: UserService,
+		private _publicationService: PublicationService
 		){
 		this.identity  = this._userService.getIdentity();
 		this.token  = this._userService.getToken();
@@ -30,8 +32,24 @@ export class SidebarComponent implements OnInit{
 		console.log('El component.sidebar ha sido cargado')
 	}
 
-	onSubmit(){
-		console.log(this.publication);
+	onSubmit(form){
+		this._publicationService.addPublication(this.token, this.publication).subscribe(
+			response => {
+				if(response.publication){
+					//this.publication = response.publication;
+					this.status = 'success';
+					form.reset();
+				}else{
+					this.status = 'error';
+				}
+				},
+				error => {
+					var errorMessage = <any>error;
+					if(errorMessage != null){
+						this.status = 'error';
+					}
+				}
+			);
 	}
 
 }
