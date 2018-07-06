ee080eaba2a7e0857cc9a5f9b407a1b24da88d8d
john <maquistemas@gmail.com>
2018-07-05 22:25:40 -0500

    Servicio de mensajes

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';//funciona luego de ejecutar: npm install --save rxjs-compat
+import { GLOBAL } from './global';
+import { Follow } from '../models/follow';
+import { Message } from '../models/message';
+
+@Injectable()
+export class MessageService{
+	public url: string;
+
+	constructor(private _http: HttpClient){
+		this.url = GLOBAL.url;
+	}
+
+	addMessage(token, message): Observable<any>{
+		let params = JSON.stringify(message);
+		let headers = new HttpHeaders().set('Content-Type', 'application/json')
+										.set('Authorization', token);
+
+		return this._http.post(this.ur+'message', params, {headers: headers}); 
+	}
+
+	getMyMessages(token, page = 1){
+		let headers = new HttpHeaders().set('Content-Type', 'application/json')
+										.set('Authorization', token);
+
+	 	return this._http.get(this.url+'my-messages/'+page, {headers: headers});
+
+	}
+
+	getEmitMessages(token, page = 1){
+		let headers = new HttpHeaders().set('Content-Type', 'application/json')
+										.set('Authorization', token);
+
+	 	return this._http.get(this.url+'messages/'+page, {headers: headers});
+
+	}
+
+
+
+
+
+}
